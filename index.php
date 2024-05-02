a021d216c33faaadfe5ebd5ebd1acd163dc7a27d
Enyi Uchechukwu Miracle <ucheenyi@gmail.com>
2024-05-02 12:10:45 +0100

    Create index.php
    index page

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+// Get the current date in YYYY-MM-DD format
+$currentDate = date('Y-m-d');
+echo "Today's date is: $currentDate<br>";
+
+// Get the current date and time in YYYY-MM-DD HH:MM:SS format
+$currentDateTime = date('Y-m-d H:i:s');
+echo "Current date and time is: $currentDateTime";
+?>
